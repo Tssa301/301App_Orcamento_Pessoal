831f0a6f23f3812b18c292e8886fe775f11ab372
Tiago Ssa <tiago.silva301@gmail.com>
2020-07-30 22:09:46 +0100

    Criada a possibilidade de remoção de despesas inseridas em local storage.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ class Bd {
               if(despesa === null) {
                   continue
               }
-
+            despesa.id = i
             despesas.push(despesa)
         }
 
@@ -114,6 +114,10 @@ class Bd {
 
         return despesasFiltradas
     }
+
+    remover(id) {
+        localStorage.removeItem(id)
+    }
 }
 
 
@@ -140,7 +144,7 @@ function cadastrarDespesa() {
     )
 
     if(despesa.validarDados()) {
-        //bd.gravar(despesa)
+        bd.gravar(despesa)
 
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!'
         document.getElementById('modal_titulo_div').className = 'modal-header text-success'
@@ -208,6 +212,23 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criar o botão exclusão
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+            //remover a despesa
+            let id = this.id.replace('id_despesa_', '')
+
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
     })
 }
 
